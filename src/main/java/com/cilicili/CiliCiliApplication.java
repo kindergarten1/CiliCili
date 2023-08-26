4913d23d9c3bf450c8039096b7290514d0d834eb
SlyAimer <2289782085@qq.com>
2023-08-26 18:33:20 +0800

    Initial commit

---
+++ src/main/java/com/cilicili/CiliCiliApplication.java
@@ -0,0 +1,13 @@
+package com.cilicili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiliCiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiliCiliApplication.class, args);
+    }
+
+}
